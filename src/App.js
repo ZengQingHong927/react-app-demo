a53cf0c052f31679577e0a6d4a7fb1a51c741c35
Nick-Zeng <nicktseng927@gmail.com>
2020-08-16 15:43:27 +0800

    fx title

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Welcome back to Taiwan! Nick~
           You will get a web developer job in Taipei in October 2020.
-          React+NodeJS+MongoDB
+          React+NodeJS+MongoDB+Redis
         </a>
       </header>
     </div>
